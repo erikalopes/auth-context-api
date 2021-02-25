ec1da89af20bb21b69829cab1095c87567d11e3f
Erika Lopes <201810099@acad.unijuazeiro.edu.br>
2021-02-25 16:05:05 -0300

    add async storage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,6 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 import * as auth from '../services/auth';
-
+import AsyncStorage from '@react-native-community/async-storage';
 interface AuthContextData {
   signed: boolean;
   user: object | null;
@@ -12,9 +12,26 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
   const [user, setUser] = useState<object | null>(null);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const storagedUser = await AsyncStorage.getItem('@RnAuth:user');
+      const storageToken = await AsyncStorage.getItem('@RnAuth:token');
+
+      if (storagedUser && storageToken) {
+        setUser(JSON.parse(storagedUser));
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
   async function signIn() {
     const response = await auth.signIn();
     setUser(response.user);
+
+    await AsyncStorage.setItem('@RnAuth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@RnAuth:token', response.token);
   }
 
   function signOut() {
